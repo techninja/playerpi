2a3a63c3ebf4e34a3f9ee7780345135dd5334088
techninja <james@tn42.com>
2014-12-29 16:52:15 -0800

    Refactor server and add simple debug mode

--- playerpi.js
+++ playerpi.js
@@ -3,7 +3,9 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var midi = require('midi');
-var port = 80;
+var debug = false;
+var httpPort = 80;
+var midiPort = 1;
 
 
 // Midi Stuff
@@ -12,29 +14,35 @@ var input = new midi.input();
 var output = new midi.output();
 
 // Get the name of a specified input port.
-console.log('Opening midi port:', input.getPortName(1));
-input.openPort(1);
-output.openPort(1);
+console.log('Opening midi port:', input.getPortName(midiPort));
+input.openPort(midiPort);
+output.openPort(midiPort);
 
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
-// Routing
+// Routing (for index.html)
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function (socket) {
 
-  // when the client emits 'new message', this listens and executes
+  // When we recieve a midiout event, push that to the midi port
   socket.on('midiout', function (data) {
-    console.log('Midi Output:', data);
+  	debugMessage('OUTPUT', data);
     output.sendMessage(data);
   });
 
-  // Configure a callback.
+  // Emit a socket for input from the piano to clients
   input.on('message', function(deltaTime, message) {
+  	debugMessage('INPUT ', message);
     socket.emit('midiin', message);
   });
 
 });
+
+// Helper func for message debug
+function debugMessage() {
+  if (debug) console.debug(arguments);
+}
